fc0eea3f251c626cfa30ebcadd528fa95609273d
Emi <178958173+gonkEnjoyer@users.noreply.github.com>
2026-05-29 23:35:12 +1000

    Refactor page loading overlay

--- website/assets/scripts/loading.js
+++ website/assets/scripts/loading.js
@@ -1,4 +1,6 @@
 const loadingScreen = document.querySelector(".loading-screen");
 if (loadingScreen) {
-    loadingScreen.style.display = "none";
+    window.addEventListener("load", () => {
+      loadingScreen.remove();
+    });
 }
